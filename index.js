c759877d5bd4003b3839b09db2b52db59ed3898f
QWAXER <mrlopy954@gmail.com>
2026-09-09 08:19:25 +0300

    fix for SB

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http'); 
 const server = http.createServer((req, res) => { 
 res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); 
-res.end('<h1>Привет, мир!</h1>'); 
+
 }); 
 const PORT = 3000;
 
