b2f4334d55c6196b07b79c6f2f38a78055b963ae
QWAXER <138833822+QWAXER@users.noreply.github.com>
2026-09-09 08:23:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ res.end('<h1>Привет, мир!</h1>');
 }); 
 const PORT = 3000;
 
-let PI = 0;
-let number = 1;
-let NumInList = 23; /*Это мой номер в списке*/
-
-for (let i = 0; i < NumInList; i++){
-    PI += 1 / number;
-    if (number > 0){
-        number += 2;
-    }
-    else {
-        number -= 2;
-    }
-    number *= -1;
-}
-
-PI *= 4;
-
 server.listen(PORT, () => { 
 console.log(`Филеня Кирилл\n478`);
 console.log(PI);
